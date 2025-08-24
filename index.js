96b50a11fb716e2c8b7a429e768a9ad5da15bbb7
Mounesh v <mouneshv696@gmail.com>
2025-08-24 13:14:07 +0530

    Image-Uploader

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/", homeRoute);
 // Upload Multer
 app.post("/upload", upload_multer, Image_Controller);
 
-const PORT  = process.env.PORT || 3000;
+const PORT  = 3000;
 app.listen(PORT , () =>
   console.log(`Server is Running On ${PORT}`)
 );
